1f8c8e2e9459ba2db2c933f1f04cd843e13bd2b3
Anna Kutsa <a.a.kuca@gmail.com>
2023-10-09 20:07:29 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import spockFascinating from './starTrek-sounds/voice_spock_fascinating.mp3';
+import spockIllogical from './starTrek-sounds/voice_spock_mostillogical.mp3';
+import spockBluff from './starTrek-sounds/voice_spock_vulcansneverbluff.mp3';
+import dataAccess from './starTrek-sounds/tng-data-ihaveaccess.mp3';
+import dataHailed from './starTrek-sounds/tng-data-beinghailed.mp3';
+import dataSystems from './starTrek-sounds/tng-data-allsystems.mp3';
+import picardStatusReport from './starTrek-sounds/tng-picard-statusreport.mp3';
+import picardEngage from './starTrek-sounds/tng-picard-engage.mp3';
+import picardMakeItSo from './starTrek-sounds/tng-picard-makeitso.mp3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const audioFiles = [
+  {id: 'Q', label: 'Spock: Fascinating', audioSrc: spockFascinating},
+  {id: 'W', label: 'Spock: Illogical', audioSrc: spockIllogical},
+  {id: 'E', label: 'Spock: Bluff', audioSrc: spockBluff},
+  {id: 'A', label: 'Data: Access', audioSrc: dataAccess},
+  {id: 'S', label: 'Data: Hailed', audioSrc: dataHailed},
+  {id: 'D', label: 'Data: Systems', audioSrc: dataSystems},
+  {id: 'Z', label: 'Picard: Engage', audioSrc: picardEngage},
+  {id: 'X', label: 'Picard: Status Report', audioSrc: picardStatusReport},
+  {id: 'C', label: 'Picard: Make It So', audioSrc: picardMakeItSo},
+
+]
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      soundId: '',
+    };
+    this.audioPlayer=[
+      React.createRef(),
+      React.createRef(),
+      React.createRef(),
+      React.createRef(),
+      React.createRef(),
+      React.createRef(),
+      React.createRef(),
+      React.createRef(),
+      React.createRef()
+    ]
+  }
+  
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = (event) => {
+    const { key } = event;
+    const button = audioFiles.find((item) => item.id === key.toUpperCase());
+    if (button) {
+      const index = audioFiles.indexOf(button);
+      this.playSound(index);
+    }
+  };
+  
+  playSound(index){
+    this.setState({
+      soundId: audioFiles[index].label
+    })
+    this.audioPlayer[index].current.play()
+  }
+  
+  render () {
+  
+    return(
+      <div id="drum-machine">
+        <div id='display'>{this.state.soundId}</div>
+        {audioFiles.map((button, index) => (
+          <button
+            key={index}
+            className='drum-pad'
+            id={button.id}
+            onClick={() => this.playSound(index)}
+          >
+            {button.id}
+            <audio
+              className='clip'
+              id={button.id}
+              src={button.audioSrc}
+              type="audio/mpeg"
+              ref={this.audioPlayer[index]}
+            />
+          </button>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
+
+
